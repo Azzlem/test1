b432dcf8ccdb4d2a8756c73a0ba5df363b16d804
Alex Sid <azzlem@mail.ru>
2023-06-11 00:45:55 +0300

    next gen2 fix

--- src/class_transation.py
+++ src/class_transation.py
@@ -1,3 +1,4 @@
+
 class Transaction():
     def __init__(self, id_, state, date, amount, name, code, description,  to_, from_=""):
         self.id = id_
@@ -9,3 +10,16 @@ class Transaction():
         self.description = description
         self.to_ = to_
         self.from_ = from_
+
+    def get_descr(self, ):
+        date = self.date[8:10] + "." + self.date[5:7] + "." + self.date[:4]
+        from_one = "" if self.from_ == None else self.from_.split()[0]
+        from_two = "" if self.from_ == None else self.from_.split()[1][:6] + 6*"*" + self.from_.split()[1][-4:]
+        to_to = 2*"*" + self.to_[-4:]
+        return f'{date} {self.description}\n' \
+               f'{from_one} {from_two} -> {to_to}\n' \
+               f'{self.amount} {self.name}\n'
+
+    def state_prof(self):
+        return True if self.state == "EXECUTED" else False
+
--- src/main.py
+++ src/main.py
@@ -1,2 +1,8 @@
 from src.utils import trans_file
+trans_file().sort(key=lambda x: x.name)
 
+for el in trans_file():
+    if el.state_prof() == True:
+        print(el.get_descr())
+
+# el.state() == True:
